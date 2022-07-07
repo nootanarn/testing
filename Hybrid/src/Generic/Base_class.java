0451930b719f09a97395424cc0bc71e7d1a2e1f2
DELL <DELL@DESKTOP-U424R0D>
2022-07-07 21:01:57 +0530

    build2

---
+++ Hybrid/src/Generic/Base_class.java
@@ -0,0 +1,12 @@
+package Generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Base_class {
+	public Base_class(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+}
